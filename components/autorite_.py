1a8d4c381f889bb3fcdfb6f83160649306d976ac
Mr-houngbo <houngbo.calixte.r@gmail.com>
2025-08-22 14:34:50 +0000

    Update autorite_.py

--- components/autorite_.py
+++ components/autorite_.py
@@ -668,6 +668,7 @@ BASE_URL = "https://api.airgradient.com/public/api/v1"
 location_id = "164928"
 location_id_input = location_id
 token_input = token
+format_output = "pdf"
 
 VALEURS_LIMITE = {
     "pm03_count": 100000,
@@ -1554,6 +1555,7 @@ def create_streamlit_interface():
             except Exception as e:
                 st.error(f"❌ Erreur lors de la génération: {str(e)}")
                 st.info("🔧 Vérifiez vos paramètres et votre connexion internet")
+
 
 
 
